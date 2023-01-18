e70e686cfd941103f94c20b7ab3881b8ac49cc1f
Jevon Cochran <jevoncochran@Jevons-MacBook-Pro.local>
2023-01-18 00:53:30 -0500

    clean up authRouter

--- routers/auth-router.js
+++ routers/auth-router.js
@@ -1,28 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-
-const users = require("../models/user-model");
 const { registerUser, loginUser } = require("../controllers/authController");
 
 router.post("/register", registerUser);
 
 router.post("/login", loginUser);
-
-const generateToken = (user) => {
-  const payload = {
-    id: user.id,
-  };
-
-  const secret = "top secret";
-
-  const options = {
-    expiresIn: "24h",
-  };
-
-  return jwt.sign(payload, secret, options);
-};
 
 module.exports = router;
